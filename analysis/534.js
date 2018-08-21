f99108e95862708150a010a30dbd7d855ac9c45d
winderica <winderica@gmail.com>
2018-08-21 21:22:34 +0800

    Update: function 534

--- analysis/534.js
+++ analysis/534.js
@@ -53,8 +53,14 @@ const function534 = function (t, e, i) {
         }
     }
 
+    /*
+     * It is interesting that the code below is compiled from Typescript,
+     * so we can believe that the HTML5 version of 艦これ is written in TS.
+     * `o` implements `extends` in TS.
+     * To understand it, there is a PERFECT answer to read:
+     * https://stackoverflow.com/a/46458070/9140339
+     */
     var o = this && this.__extends
-        // IIFE
         || function () {
             var t = Object.setPrototypeOf || { __proto__: [] } instanceof Array && function (t, e) {
                 t.__proto__ = e
@@ -71,7 +77,8 @@ const function534 = function (t, e, i) {
         }();
     Object.defineProperty(e, "__esModule", { value: !0 });
     var r = i(0), s = i(7), a = i(34), _ = i(535), u = i(540), l = i(541), c = i(2), h = i(542), p = i(270),
-        d = i(545), f = i(547), y = i(548), v = i(24), g = i(550), m = i(637), b = function (t) {
+        d = i(545), f = i(547), y = i(548), v = i(24), g = i(550), m = i(637),
+        b = function (t) {
             function e(e) {
                 var i = t.call(this) || this;
                 return i._end_titlecall = !1, i._TutorialScene = null, i._isTutorialEnd = !1, i._tutorialEnd = function () {
@@ -79,9 +86,11 @@ const function534 = function (t, e, i) {
                 }, i._end_connect_api = !1, p.TaskSceneChange.__factory__ = n, r.default.settings.initialize(e), r.default.view.initialize(new g.PortScene), i
             }
 
-            return o(e, t), e.prototype._start = function () {
+            o(e, t);
+            e.prototype._start = function () {
                 this._loadVersionFile()
-            }, e.prototype._loadVersionFile = function () {
+            };
+            e.prototype._loadVersionFile = function () {
                 var t = this, e = r.default.settings.version, i = "./version.json" + (e ? "?" + e : "");
                 axios.get(i).then(function (e) {
                     var i = s.ObjUtil.getObject(e, "data");
@@ -89,48 +98,66 @@ const function534 = function (t, e, i) {
                 }).catch(function () {
                     r.default.view.showError("Load Error.version.json.")
                 })
-            }, e.prototype._loadTitleResource = function () {
+            };
+            e.prototype._loadTitleResource = function () {
                 var t = this;
                 (new f.TaskLoadResourcesTitle).start(function () {
                     t._showTitle()
                 })
-            }, e.prototype._showTitle = function () {
+            };
+            e.prototype._showTitle = function () {
+                // `ぷかぷか` is the loading interface when entering game (小黑船 in Chinese)
                 this._pukapuka = new y.PukapukaLayer, this._pukapuka.initialize(), this._pukapuka.visible = !1, r.default.view.addChild(this._pukapuka);
                 var t = new h.TitleViewMain;
                 t.initialize(), r.default.view.addChild(t), this._loadCommonResources(t)
-            }, e.prototype._loadCommonResources = function (t) {
+            };
+            e.prototype._loadCommonResources = function (t) {
                 var e = this;
                 new d.TaskLoadResourcesCommon(function (e) {
                     t.setProgress(e)
                 }).start(function () {
                     e._showStartPage(t)
                 })
-            }, e.prototype._showStartPage = function (t) {
+            };
+            // start page is the welcome interface
+            e.prototype._showStartPage = function (t) {
                 var e = this;
                 t.showSecondPageTween(function () {
                     t.waitClickTween(function () {
                         e._playTitleCall(), e._hideTitle(t)
                     })
                 })
-            }, e.prototype._playTitleCall = function () {
+            };
+            /*
+             * title call is
+             * ①: `行きますよ、暁の水平線に、勝利を刻みなさい！`
+             * and
+             * ②: `提督が、鎮守府に着任しました、これより、艦隊の指揮に入ります！`
+             */
+            e.prototype._playTitleCall = function () {
+                // there are 84 kinds of voice ①!!!
                 var t = this, e = Math.floor(84 * Math.random()) + 1;
                 r.default.sound.voice.play("titlecall_1", e, function () {
+                    // and 47 kinds of voice ②!
                     var e = Math.floor(47 * Math.random()) + 1;
                     r.default.sound.voice.play("titlecall_2", e, function () {
                         t._end_titlecall = !0, t._preInitPort()
                     })
                 })
-            }, e.prototype._hideTitle = function (t) {
+            };
+            e.prototype._hideTitle = function (t) {
                 var e = this;
                 this._pukapuka.visible = !0, this._pukapuka.activate(), r.default.view.portMain.visible = !1, createjs.Tween.get(t).to({ alpha: 0 }, 500).call(function () {
                     r.default.view.removeChild(t), t.dispose(), e._connectAPI()
                 })
-            }, e.prototype._connectAPI = function () {
+            };
+            e.prototype._connectAPI = function () {
                 var t = this;
                 (new a.APIConnector).add(new l.GetIncentiveAPI).add(new _.GetDataAPI).add(new u.RequireInfoAPI).start(function () {
                     t._end_connect_api = !0, r.default.model.basic.firstFlag ? (t._TutorialScene = new m.TutorialScene, t._TutorialScene.visible = !1, t._TutorialScene.alpha = 0, t._TutorialScene.emitter.on("tutorial-event-end", t._tutorialEnd), t._isTutorialEnd = !1, t._preInitPort()) : (t._isTutorialEnd = !0, t._preInitPort())
                 })
-            }, e.prototype._preInitPort = function () {
+            };
+            e.prototype._preInitPort = function () {
                 var t = this;
                 if (0 != this._end_titlecall && 0 != this._end_connect_api) {
                     if (!1 === this._isTutorialEnd) return r.default.view.addChild(this._TutorialScene), this._TutorialScene.visible = !0, createjs.Tween.get(this._TutorialScene).to({ alpha: 1 }, 600), !1;
@@ -138,7 +165,8 @@ const function534 = function (t, e, i) {
                         t._hidePukapuka()
                     })
                 }
-            }, e.prototype._hidePukapuka = function () {
+            };
+            e.prototype._hidePukapuka = function () {
                 var t = this;
                 r.default.view.portMain.fadeLayer.alpha = 1, r.default.view.portMain.fadeLayer.visible = !0, r.default.view.portMain.visible = !0, createjs.Tween.get(this._pukapuka).to({ alpha: 0 }, 600).call(function () {
                     r.default.view.removeChild(t._pukapuka), t._pukapuka.dispose(), t._pukapuka = null;
@@ -147,12 +175,14 @@ const function534 = function (t, e, i) {
                         r.default.view.portMain.fadeLayer.visible = !1, e.startAnimation(), r.default.view.portMain.sideMenu.initialize()
                     }), t._initPort()
                 })
-            }, e.prototype._initPort = function () {
+            };
+            e.prototype._initPort = function () {
                 var t = this;
                 r.default.view.getNowScene().getInitializeTask(0).start(function () {
                     t._endTask()
                 })
-            }, e
+            };
+            return e;
         }(c.TaskBase);
     e.AppInitializeTask = b;
     var w = i(649), x = i(732), I = i(751), T = i(826), O = i(851), P = i(901), C = i(1039), k = i(1055),
