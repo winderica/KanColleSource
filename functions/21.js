ca0c458000dff1a13aa7f794cb86630775f89b13
winderica <winderica@gmail.com>
2018-08-22 18:21:40 +0800

    Update: `main.js` version 4.0.1.9

--- functions/21.js
+++ functions/21.js
@@ -1,43 +1,38 @@
 const function21 = function (t, e, i) {
     "use strict";
-    Object.defineProperty(e, "__esModule", { value: !0 });
-    var n = i(0), o = function () {
-        function t(e) {
-            var i = this;
-            if (this._cb_onProgress = null, this._onLoadComplete = function (t, e) {
-                i._cb_onComplete(t, e), i._cb_onComplete = null, null != i._cb_onProgress && i._loader.off("progress", i._cb_onProgress), i._cb_onProgress = null, i._keys = null, i._loader = null
-            }, this._category = e, this._keys = [], this._loader = new PIXI.loaders.Loader, null != t._version_data && t._version_data[e]) {
-                var n = t._version_data[e];
-                this._loader.defaultQueryString = "version=" + n
+    var n = this && this.__extends || function () {
+        var t = Object.setPrototypeOf || { __proto__: [] } instanceof Array && function (t, e) {
+            t.__proto__ = e
+        } || function (t, e) {
+            for (var i in e) e.hasOwnProperty(i) && (t[i] = e[i])
+        };
+        return function (e, i) {
+            function n() {
+                this.constructor = e
             }
+
+            t(e, i), e.prototype = null === i ? Object.create(i) : (n.prototype = i.prototype, new n)
+        }
+    }();
+    Object.defineProperty(e, "__esModule", { value: !0 });
+    var o = function (t) {
+        function e() {
+            return null !== t && t.apply(this, arguments) || this
         }
 
-        return t.setVersionData = function (t) {
-            this._version_data = t
-        }, t.getPath = function (t, e) {
-            return null == e || "" == e ? n.default.settings.path_root + "img/" + t : n.default.settings.path_root + "img/" + e + "/" + t
-        }, t.clearMemoryCache = function (e) {
-            var i = new RegExp("^img\\/" + e + "\\/.+");
-            t._clearMemoryCache(i);
-            var n = new RegExp("^" + e + "_.+_\\d+");
-            t._clearMemoryCache(n)
-        }, t._clearMemoryCache = function (e) {
-            for (var i = Object.keys(PIXI.utils.TextureCache).filter(function (t) {
-                return e.test(t)
-            }), n = 0, o = i; n < o.length; n++) {
-                var r = o[n], s = PIXI.utils.TextureCache[r];
-                s && s.destroy(!0), delete PIXI.utils.TextureCache[r]
-            }
-            t._loaded = t._loaded.filter(function (t) {
-                return 0 == e.test(t)
-            })
-        }, t.prototype.add = function (e, i) {
-            void 0 === i && (i = null);
-            var n = t.getPath(e, this._category);
-            return null == t._loaded && (t._loaded = []), t._loaded.indexOf(n) >= 0 ? this : (t._loaded.push(n), this._keys.push(n), this._loader = null == i ? this._loader.add(n) : this._loader.add(i, n), this)
-        }, t.prototype.load = function (t, e) {
-            return void 0 === t && (t = null), void 0 === e && (e = null), this._cb_onComplete = t, this._cb_onProgress = e, null != e && this._loader.on("progress", this._cb_onProgress), this._keys.length > 0 ? this._loader = this._loader.load(this._onLoadComplete) : this._onLoadComplete(this._loader, null), this
-        }, t._version_data = null, t
-    }();
-    e.UIImageLoader = o
+        return n(e, t), Object.defineProperty(e.prototype, "scaleX", {
+            get: function () {
+                return this.scale.x
+            }, set: function (t) {
+                this.scale.x = t
+            }, enumerable: !0, configurable: !0
+        }), Object.defineProperty(e.prototype, "scaleY", {
+            get: function () {
+                return this.scale.y
+            }, set: function (t) {
+                this.scale.y = t
+            }, enumerable: !0, configurable: !0
+        }), e
+    }(PIXI.Container);
+    e.Container = o
 }
--- functions/24.js
+++ functions/24.js
@@ -1,36 +1,28 @@
 const function24 = function (t, e, i) {
     "use strict";
     Object.defineProperty(e, "__esModule", { value: !0 });
-    var n = i(0), o = i(260), r = i(23), s = i(102), a = i(76), _ = function () {
-        function t() {
-            null == t._queue && (t._queue = []), this._url_list = []
+    var n = i(0), o = function () {
+        function t(e) {
+            var i = this;
+            if (this._cb_onProgress = null, this._onLoadComplete = function (t, e) {
+                i._cb_onComplete(t, e), i._cb_onComplete = null, null != i._cb_onProgress && i._loader.off("progress", i._cb_onProgress), i._cb_onProgress = null, i._keys = null, i._loader = null
+            }, this._category = e, this._keys = [], this._loader = new PIXI.loaders.Loader, null != t._version_data && t._version_data[e]) {
+                var n = t._version_data[e];
+                this._loader.defaultQueryString = "version=" + n
+            }
         }
 
-        return t.getPath = function (t, e) {
-            var i = "slot_" + e, o = s.SuffixUtil.create(t, i), _ = r.MathUtil.zeroPadding(t, 3);
-            return n.default.settings.path_root + "resources/slot/" + e + "/" + _ + "_" + o + ".png" + a.VersionUtil.getResourceVersion(1, parseInt(_))
+        return t.setVersionData = function (t) {
+            this._version_data = t
+        }, t.getPath = function (t, e) {
+            return null == e || "" == e ? n.default.settings.path_root + "img/" + t : n.default.settings.path_root + "img/" + e + "/" + t
         }, t.prototype.add = function (e, i) {
-            "item_up" == i && (1 == o.ITEMUP_REPLACE.hasOwnProperty(e.toString()) ? e = o.ITEMUP_REPLACE[e] : e > 500 && (e -= 500));
-            var n = t.getPath(e, i);
-            return this._url_list.push(n), this
-        }, t.prototype.load = function (e) {
-            void 0 === e && (e = null), this._cb_onComplete = e, t._queue.indexOf(this) >= 0 || (t._queue.push(this), 1 == t._queue.length && this._load())
-        }, t.prototype._load = function () {
-            var t = this;
-            if (this._url_list = this._url_list.filter(function (t, e, i) {
-                return !PIXI.utils.TextureCache[t] && i.indexOf(t) === e
-            }), 0 == this._url_list.length) return void this._onLoadComplete();
-            for (var e = new PIXI.loaders.Loader, i = 0, n = this._url_list; i < n.length; i++) {
-                var o = n[i];
-                e.add(o)
-            }
-            e.load(function () {
-                t._onLoadComplete()
-            })
-        }, t.prototype._onLoadComplete = function () {
-            if (this._cb_onComplete(), this._cb_onComplete = null, t._queue.shift() != this) throw new Error("SlotLoader Error");
-            t._queue.length > 0 && t._queue[0]._load()
-        }, t
+            void 0 === i && (i = null);
+            var n = t.getPath(e, this._category);
+            return null == t._loaded && (t._loaded = []), -1 == t._loaded.indexOf(n) && (t._loaded.push(n), this._keys.push(n), this._loader = null == i ? this._loader.add(n) : this._loader.add(i, n)), this
+        }, t.prototype.load = function (t, e) {
+            return void 0 === t && (t = null), void 0 === e && (e = null), this._cb_onComplete = t, this._cb_onProgress = e, null != e && this._loader.on("progress", this._cb_onProgress), this._keys.length > 0 ? this._loader = this._loader.load(this._onLoadComplete) : this._onLoadComplete(this._loader, null), this
+        }, t._version_data = null, t
     }();
-    e.SlotLoader = _
+    e.UIImageLoader = o
 }
