d2d10049fe7726c499e717776ce62594eb07ecf2
winderica <winderica@gmail.com>
2018-08-24 15:45:22 +0800

    Add: dependency tree

---
+++ utils/searcher.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const searched = [];
+
+const searcher = index => {
+    try {
+        const res = fs.readFileSync(`../functions/${index}.js`);
+        // In bundled code, 1000 is 1e3
+        const imports = res.toString().match(/i\(\d+\)|i\(1e3\)/g);
+        const tree = { [index]: null };
+        if (searched[index]) {
+            return tree;
+        }
+        searched[index] = true;
+        if (!imports) {
+            return tree;
+        }
+        const thousand = imports.indexOf('i(1e3)');
+        if (thousand >= 0) {
+            imports[thousand] = 'i(1000)';
+        }
+        const results = imports.map(i => searcher(i.match(/\d+/)[0]));
+        results.map(i => tree[index] ? tree[index][Object.keys(i)[0]] = Object.values(i)[0] : tree[index] = i);
+        return tree;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+fs.writeFile('../tree.json', JSON.stringify(searcher(475)), err => {
+    err && console.log(err);
+});
+
+for (let i = 0; i < searched.length; i++) {
+    if (!searched[i]) {
+        console.log(i)
+    }
+}
