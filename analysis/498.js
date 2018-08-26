1a4b1b2858a4ade4caead825c56cb7c6a5db3761
winderica <winderica@gmail.com>
2018-08-26 13:48:55 +0800

    Update: function 498, 499

--- analysis/498.js
+++ analysis/498.js
@@ -1,20 +1,27 @@
+/*
+ * the function called by `476.js`
+ * log
+ */
 const function498 = function (t, e, i) {
     "use strict";
     Object.defineProperty(e, "__esModule", { value: !0 });
-    var n = i(499), o = function () {
-        function t() {
-        }
-
-        return Object.defineProperty(t.prototype, "list", {
-            get: function () {
-                return this._list
-            }, enumerable: !0, configurable: !0
-        }), t.prototype.setData = function (t) {
-            if (this._list = [], null != t) for (var e = 0; e < t.length; e++) {
-                var i = t[e];
-                null != i && this._list.push(new n.LogModel(i))
+    var n = i(499),
+        o = function () {
+            function t() {
             }
-        }, t
-    }();
+
+            Object.defineProperty(t.prototype, "list", {
+                get: function () {
+                    return this._list
+                }, enumerable: !0, configurable: !0
+            });
+            t.prototype.setData = function (t) {
+                if (this._list = [], null != t) for (var e = 0; e < t.length; e++) {
+                    var i = t[e];
+                    null != i && this._list.push(new n.LogModel(i))
+                }
+            };
+            return t
+        }();
     e.LogModelHolder = o
 }
