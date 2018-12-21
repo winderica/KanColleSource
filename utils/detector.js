6790510b05d4c2e5dda6a969cf0e0527e0092061
winderica <winderica@gmail.com>
2018-12-21 16:14:18 +0800

    Update: auto push

--- utils/detector.js
+++ utils/detector.js
@@ -1,22 +1,16 @@
 const cron = require('cron');
 const fetch = require('node-fetch');
 const fs = require('fs');
-const detector = cron.job("0 0 * * * *", function () {
-    fetch('http://203.104.209.7/gadget_html5/js/kcs_const.js')
-        .then(res => res.text())
-        .catch(err => console.log(err))
-        .then(res => {
-            const version = res.match(/\d\.\d\.\d\.\d/)[0];
-            fetch(`http://203.104.209.71/kcs2/js/main.js?version=${version}`)
-                .then(res => res.text())
-                .catch(err => console.log(err))
-                .then(res => {
-                    fs.access(`../raw-${version}.js`, fs.constants.F_OK, err => {
-                        err && fs.writeFile(`../raw-${version}.js`, res, err => {
-                            err && console.log(err);
-                        });
-                    });
-                })
-        });
+const { exec } = require('child_process');
+
+const detector = cron.job("0 0 * * * *", async () => {
+    const gameConsts = await (await fetch('http://203.104.209.7/gadget_html5/js/kcs_const.js')).text();
+    const version = gameConsts.match(/\d\.\d\.\d\.\d/)[0];
+    const script = await (await fetch(`http://203.104.209.71/kcs2/js/main.js?version=${version}`)).text();
+    fs.writeFile(`../raw.js`, script, err => {
+        if (!err) {
+            exec(`git commit -m 'Update: \`main.js\` v${version}'`);
+        }
+    });
 });
 detector.start();
