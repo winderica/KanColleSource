2d8d4bf7359792be87e61b254fca264cb64afd63
winderica <winderica@gmail.com>
2018-12-21 16:45:44 +0800

    Update: improve auto pushing

--- utils/detector.js
+++ utils/detector.js
@@ -4,15 +4,20 @@ const fs = require('fs');
 const { exec } = require('child_process');
 
 const detector = cron.job("0 0 * * * *", async () => {
-    const gameConsts = await (await fetch('http://203.104.209.7/gadget_html5/js/kcs_const.js')).text();
-    const version = gameConsts.match(/\d\.\d\.\d\.\d/)[0];
-    const script = await (await fetch(`http://203.104.209.71/kcs2/js/main.js?version=${version}`)).text();
-    fs.writeFile(`../raw.js`, script, err => {
-        if (!err) {
-            exec('git add raw.js');
+    try {
+        const gameConsts = await (await fetch('http://203.104.209.7/gadget_html5/js/kcs_const.js')).text();
+        const version = gameConsts.match(/\d\.\d\.\d\.\d/)[0];
+        const currentVersion = fs.readFileSync('../version').toString();
+        if (version !== currentVersion) {
+            const script = await (await fetch(`http://203.104.209.71/kcs2/js/main.js?version=${version}`)).text();
+            fs.writeFileSync(`../raw.js`, script);
+            fs.writeFileSync('../version', version);
+            exec('git add ../raw.js');
             exec(`git commit -m "Update: \`raw.js\` v${version}"`);
             exec('git push origin master');
         }
-    });
+    } catch (e) {
+        console.log(e);
+    }
 });
 detector.start();
