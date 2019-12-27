c57aa790e9e1c106308c336b45ded4b3457d51b2
winderica <winderica@gmail.com>
2019-12-27 17:30:08 +0900

    Update: main.js v4.5.2.0

--- functions/244.js
+++ functions/244.js
@@ -2,5 +2,5 @@ const function244 = function (t, e, i) {
     "use strict";
     Object.defineProperty(e, "__esModule", {
         value: !0
-    }), e.POSTER_GIRL = "xmas", e.POSTER_KEY_1 = "duty_poster_1", e.POSTER_KEY_2 = "duty_poster_2", e.BUTAI_SAIHENSEI = [626, 629, 630, 659, 660]
+    }), e.POSTER_GIRL = "", e.POSTER_KEY_1 = "duty_poster_1", e.POSTER_KEY_2 = "duty_poster_2", e.BUTAI_SAIHENSEI = [626, 629, 630, 659, 660]
 }
